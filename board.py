b63b8aa4e48810573ca3c42d6d9149aad88240f8
matimansilla <Matias Mansilla>
2024-08-17 23:50:30 -0300

    Update, Add test_rook

--- board.py
+++ board.py
@@ -1,18 +1,17 @@
-from rook import Rook
+from pieces import Rook
 
 class Board:
     def __init__(self):
-        self.__positions__ = []
-
+        self.__positions__=[]
         for _ in range(8):
-            col = []
+            col=[]
             for _ in range(8):
                 col.append(None)
             self.__positions__.append(col)
-        self.__positions__[0][0] = Rook("BLACK") # Black
-        self.__positions__[0][7] = Rook("BLACK") # Black
-        self.__positions__[7][7] = Rook("WHITE") # White
-        self.__positions__[7][0] = Rook("WHITE") # White
-
-    def get_piece(self, row, col):
+        self.__positions__[0][0]=Rook('BLACK','rook1_black')
+        self.__positions__[0][7]=Rook('BLACK','rook2_black')
+        self.__positions__[7][7]=Rook('WHITE','rook1_white')
+        self.__positions__[7][0]=Rook('WHITE','rook2_white')
+    
+    def get_piece(self,row,col):
         return self.__positions__[row][col]
