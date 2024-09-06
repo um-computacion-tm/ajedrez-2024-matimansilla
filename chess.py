c059fa9539fdad2be2eb3b6b335cf0f8e5c499d7
matimansilla <Matias Mansilla>
2024-09-06 00:54:58 -0300

    chess.py, cli.py

--- chess.py
+++ chess.py
@@ -1,20 +1,41 @@
 from board import Board
+from exceptions import InvalidMove, InvalidTurn, EmptyPosition
 
 class Chess:
     def __init__(self):
         self.__board__ = Board()
         self.__turn__ = "WHITE"
-
-    def move(self, from_row, from_col, to_row, to_col):
+    
+    def is_playing(self):
+        return True
+    
+    def move(
+        self,
+        from_row,
+        from_col,
+        to_row,
+        to_col,
+    ):
+        
         piece = self.__board__.get_piece(from_row, from_col)
-        self.__board__.move_piece(from_row, from_col, to_row, to_col)
+        if not piece:
+            raise EmptyPosition()
+        if not piece.get_color() == self.__turn__:
+            raise InvalidTurn()
+        if not piece.valid_positions(from_row, from_col, to_row, to_col):
+            raise InvalidMove()
+        self.__board__.move(from_row, from_col, to_row, to_col)
         self.change_turn()
+
+
+    def turn(self):
+        return self.__turn__
+    
+    def show_board(self):
+        return str(self.__board__)
 
     def change_turn(self):
         if self.__turn__ == "WHITE":
             self.__turn__ = "BLACK"
         else:
             self.__turn__ = "WHITE"
-
-    def turn(self):
-        return self.__turn__
--- cli.py
+++ cli.py
@@ -1,24 +1,33 @@
-from chess import Chess 
+from chess import Chess
+from exceptions import InvalidMove, InvalidTurn, EmptyPosition
 
-def main():
+def main ():
     chess = Chess()
     while True:
         play(chess)
 
 def play(chess):
     try:
-        from_row = int(input("From row: "))
-        from_col = int(input("From col: "))
-        to_row = int(input("To row: "))
-        to_col = int(input("To col: "))
+        print(chess.show_board())
+        print("turn: ", chess.turn)
+        from_row= int(input("From row: "))
+        from_col= int(input("From col: "))
+        to_row= int(input("To row: "))
+        to_col= int(input("To col: "))
     
-        chess.move(from_row, from_col, to_row, to_col)
-        print("Move successful")  # Agregado para el caso feliz
+        chess.move(
+            from_row,
+            from_col,
+            to_row,
+            to_col
+        )
     
-    except ValueError:
-        print("Invalid input. Please enter a valid move.")
+    except InvalidMove as e:
+        print(e)
     except Exception as e:
-        print("Error:", e)
+        print("error", e)
+
 
 if __name__ == '__main__':
     main()
+    
