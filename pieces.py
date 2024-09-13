37b26aab2cbad9748b6c53ce41debf3cb4a42e8b
matimansilla <Matias Mansilla>
2024-09-12 23:30:32 -0300

    Add knight.py, Update pieces.py

--- pieces.py
+++ pieces.py
@@ -57,5 +57,18 @@ class Rook(Piece):
 class Pawn(Piece):
     def __init__(self, color, board):
         super().__init__(color, board)
-        self.white_str = "♙"
-        self.black_str = "♟"
+        self.white_str = "♙"  # Símbolo para WHITE
+        self.black_str = "♟"  # Símbolo para BLACK
+
+
+class Knight(Piece):
+    def __init__(self, color, board):
+        super().__init__(color, board)
+        self.white_str = "♘"  # Símbolo para el caballo blanco
+        self.black_str = "♞"  # Símbolo para el caballo negro
+        
+    def is_valid_piece_move(self, board, from_row, from_col, to_row, to_col):
+        """Verifica si el movimiento es válido para el caballo."""
+        # El caballo se mueve en forma de "L"
+        return (abs(from_row - to_row) == 2 and abs(from_col - to_col) == 1) or \
+               (abs(from_row - to_row) == 1 and abs(from_col - to_col) == 2)
