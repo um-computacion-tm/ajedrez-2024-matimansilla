51d974afa82f5bf5debc9760336fb8a63b2a9dae
matimansilla <Matias Mansilla>
2024-10-22 21:02:21 -0300

    Update exceptions.py, test_board.py

---
+++ CHANGELOG.md
@@ -0,0 +1,179 @@
+# CHANGELOG
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+
+## [1.0.0] - 2024-10-14
+### Fixed
+- Chess game is finished!!!!!
+
+## [0.33.0] - 2024-10-13
+### Fixed
+- Fixed CodeClimate issues and finished testing
+
+
+## [0.32.0] - 2024-10-08
+### Added
+- Rook and Pawn testing and implementation
+
+## [0.36.0] - 2024-10-06
+### Fixed
+- Fixed Exceptions
+
+## [0.35.0] - 2024-10-05
+### Fixed
+- Refactor chess move validations
+- Tested chess 
+
+## [0.34.0] - 2024-10-03
+### Added
+- Added tests to board implementation
+
+## [0.33.0] - 2024-10-02
+### Added
+- Added chess testing to move validations in chess
+
+## [0.32.0] - 2024-10-01
+### Added
+- Added score attribute to piece implementation and get_score method
+- Added piece score to board testing and implementation
+
+## [0.31.0] - 2024-09-30
+### Fixed
+- Rook implementation
+- Rook testing
+
+## [0.30.0] - 2024-09-28
+### Added
+- Added testing to new chess implementation
+
+## [0.29.0] - 2024-09-28
+### Fixed
+- Fixed Chess - added validations to move method in chess
+
+## [0.28.0] - 2024-09-27
+### Fixed
+- Refactor Chess 
+
+## [0.27.0] - 2024-09-26
+### Fixed
+- Board implementation 
+
+## [0.26.0] - 2024-09-20
+### Added
+- Player implementation
+
+## [0.25.0] - 2024-09-15
+### Fixed
+- Board implementation
+- Board testing
+
+
+## [0.24.0] - 2024-09-10
+### Fixed
+- Board testing
+- Chess testing
+- CLI testing
+- Rook testing
+
+## [0.23.0] - 2024-09-08
+### Fixed
+- Fixed board and cli implementation.
+- Fixed OutOfBoard exception message
+
+## [0.22.0] - 2024-09-07
+### Added
+- Added piece out of board exception.
+
+## [0.21.0] - 2024-09-06
+### Added
+- Added chess exceptions.
+
+## [0.20.0] - 2024-09-05
+### Added
+- Added get color method in board.
+
+## [0.19.0] - 2024-09-04
+### Added
+- Added move method in board.
+
+## [0.18.0] - 2024-09-03
+### Added
+- Added bishop movements and testing.
+
+## [0.17.0] - 2024-09-02
+### Added
+- Tested piece implementation.
+
+## [0.16.0] - 2024-09-01
+### Added
+- Added tower movements and testing.
+
+## [0.15.0] - 2024-08-29
+### Fixed
+- Fixed board and added pieces in initial position.
+
+### Added
+- Added rook testing
+
+## [0.14.0] - 2024-08-27
+### Added
+- Implemented rook vertical movement
+
+## [0.13.0] - 2024-08-25
+### Fixed
+- Fixed board testing and added place piece method in board file.
+
+## [0.120] - 2024-08-24
+### Fixed
+- Fixed validate_input method in chess file and tested cli.
+
+## [0.11.0] - 2024-08-24
+### Added
+- Added is_playing and board_display testing to chess implementation.
+
+
+## [0.10.0] - 2024-08-23
+### Fixed
+- Fixed cli and cli testing.
+
+## [0.9.0] - 2024-08-22
+### Fixed
+- Fixed board.
+
+## [0.8.0] - 2024-08-21
+### Fixed
+- Fixed cli and cli testing.
+
+## [0.7.0] - 2024-08-20
+### Added
+- Added cli testing.
+
+## [0.6.0] - 2024-08-19
+### Added
+- Added pawn testing.
+## [0.5.0] - 2024-08-17
+### Added
+- Added rook testing.
+
+### Fixed
+- Fixed board.
+
+## [0.4.0] - 2024-08-16
+### Added
+- Added board testing.
+
+## [0.3.0] - 2024-08-15
+### Added
+- Added pawn, rook and chess testing.
+
+## [0.2.0] - 2024-08-14
+### Added
+- Added CircleCI badges, Added basic structure and documentation.
+
+## [0.1.0] - 2024-08-13
+### Added
+- Added basic structure and documentation.
--- test_board.py
+++ test_board.py
@@ -1,150 +1,151 @@
+import sys
+import os
 import unittest
+
+# Asegura que se puedan importar correctamente los módulos en directorios superiores
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from board import Board
 from pieces import Pawn
 from rook import Rook
+from knight import Knight
+from bishop import Bishop
+from queen import Queen
+from king import King
 from exceptions import OutOfBoard, RowOutOfBoard, ColumnOutOfBoard
+
 
 class TestBoard(unittest.TestCase):
 
-    # STR
+    def setUp(self):
+        self.board = Board()
+        self.pawn_white1 = Pawn("WHITE", self.board)
+        self.pawn_white2 = Pawn("WHITE", self.board)
+        self.pawn_black = Pawn("BLACK", self.board)
+        # Coloca algunas piezas en el tablero
+        self.board.set_piece(0, 0, self.pawn_white1)
+        self.board.set_piece(1, 1, self.pawn_black)
+        self.board.set_piece(0, 1, self.pawn_white2)
 
-    def test_str_board(self):
-        board = Board()
-        board.rook_positions()  # Coloca las torres en el tablero
-        board.pawn_positions()  # Coloca los peones en el tablero
-        self.assertEqual(
-            str(board),
-            (
-                "♜......♜\n"
-                "♟♟♟♟♟♟♟♟\n"
-                "........\n"
-                "........\n"
-                "........\n"
-                "........\n"
-                "♙♙♙♙♙♙♙♙\n"
-                "♖......♖\n"  # Añadido el salto de línea final
-            )
-        )
+    # Cobertura de Casos Generales de Funcionalidad
 
-    # GET_PIECE METHOD
+    def test_get_cell_string_empty(self):
+        cell = None
+        expected_output = " "
+        self.assertEqual(self.board.get_cell_string(cell), expected_output)
 
-    def test_get_piece_in_range(self):
-        board = Board()
-        board.rook_positions()
-        self.assertIsInstance(board.get_piece(0, 0), Rook)
-        self.assertIsNone(board.get_piece(2, 0))
+    def test_get_cell_string_with_piece(self):
+        pawn = Pawn("WHITE", self.board)
+        self.board.set_piece(0, 0, pawn)  # Coloca un peón en la celda (0, 0)
+        expected_output = pawn.symbol()  # Se espera que devuelva el símbolo del peón
+        self.assertEqual(self.board.get_cell_string(self.board.get_piece(0, 0)), expected_output)
+
+    def test_move_valid(self):
+        # Mueve el peón de (1, 1) a (2, 1)
+        self.board.move(1, 1, 2, 1)
+        self.assertEqual(self.board.get_piece(2, 1), self.pawn_black)
+        self.assertIsNone(self.board.get_piece(1, 1))  # Verifica que la antigua posición esté vacía
+
+    def test_count_white_pieces(self):
+        # Verifica que el conteo de piezas blancas sea correcto
+        self.assertEqual(self.board.count_pieces("WHITE"), 18)
+
+    def test_count_black_pieces(self):
+        # Verifica que el conteo de piezas negras sea correcto
+        self.assertEqual(self.board.count_pieces("BLACK"), 14)
+
+    def test_count_no_pieces(self):
+        # Verifica el conteo cuando no hay piezas de un color en el tablero
+        self.assertEqual(self.board.count_pieces("RED"), 0)
+
+    # Cobertura de Casos de Error (Manejo de Excepciones)
 
     def test_get_piece_out_of_range_row_and_col(self):
-        board = Board()
         with self.assertRaises(OutOfBoard) as exc:
-            board.get_piece(10, 10)
+            self.board.get_piece(10, 10)
         self.assertEqual(
             exc.exception.message,
             "La posición (10, 10) se encuentra fuera del tablero."
         )
 
     def test_get_piece_out_of_range_row(self):
-        board = Board()
         with self.assertRaises(RowOutOfBoard) as exc:
-            board.get_piece(10, 0)
+            self.board.get_piece(10, 0)
         self.assertEqual(
             exc.exception.message,
             "La fila indicada se encuentra fuera del tablero."
         )
 
     def test_get_piece_out_of_range_col(self):
-        board = Board()
         with self.assertRaises(ColumnOutOfBoard) as exc:
-            board.get_piece(0, 10)
+            self.board.get_piece(0, 10)
         self.assertEqual(
             exc.exception.message,
             "La columna indicada se encuentra fuera del tablero."
         )
 
-    # SET_PIECE METHOD
-
-    def test_set_piece_in_range(self):
-        board = Board()
-        rook = Rook('BLACK', board)
-        board.set_piece(0, 0, rook)
-        self.assertIsInstance(board.get_piece(0, 0), Rook)
-        self.assertEqual(board.get_piece(0, 0).color, 'BLACK')
-
     def test_set_piece_out_of_range_row_and_col(self):
-        board = Board()
-        rook = Rook('BLACK', board)
+        rook = Rook('BLACK', self.board)
         with self.assertRaises(OutOfBoard) as exc:
-            board.set_piece(10, 10, rook)
+            self.board.set_piece(10, 10, rook)
         self.assertEqual(
             exc.exception.message,
             "La posición (10, 10) se encuentra fuera del tablero."
         )
 
     def test_set_piece_out_of_range_row(self):
-        board = Board()
-        rook = Rook('BLACK', board)
+        rook = Rook('BLACK', self.board)
         with self.assertRaises(RowOutOfBoard) as exc:
-            board.set_piece(10, 0, rook)
+            self.board.set_piece(10, 0, rook)
         self.assertEqual(
             exc.exception.message,
             "La fila indicada se encuentra fuera del tablero."
         )
 
     def test_set_piece_out_of_range_col(self):
-        board = Board()
-        rook = Rook('BLACK', board)
+        rook = Rook('BLACK', self.board)
         with self.assertRaises(ColumnOutOfBoard) as exc:
-            board.set_piece(0, 10, rook)
+            self.board.set_piece(0, 10, rook)
         self.assertEqual(
             exc.exception.message,
             "La columna indicada se encuentra fuera del tablero."
         )
 
-    # PUTTING ROOKS ON THE BOARD WHEN STARTING THE GAME
+    # Pruebas adicionales del Estado Inicial del Tablero y Movimientos
 
     def test_rooks_creation(self):
-        board = Board()
-        board.rook_positions()  # Coloca las torres
-        self.assertIsInstance(board.get_piece(0, 0), Rook)
-        self.assertEqual(board.get_piece(0, 0).color, 'BLACK')
-        self.assertIsInstance(board.get_piece(0, 7), Rook)
-        self.assertEqual(board.get_piece(0, 7).color, 'BLACK')
-        self.assertIsInstance(board.get_piece(7, 0), Rook)
-        self.assertEqual(board.get_piece(7, 0).color, 'WHITE')
-        self.assertIsInstance(board.get_piece(7, 7), Rook)
-        self.assertEqual(board.get_piece(7, 7).color, 'WHITE')
-
-    # PUTTING PAWNS ON THE BOARD WHEN STARTING THE GAME
+        self.board.rook_positions()  # Coloca las torres
+        self.assertIsInstance(self.board.get_piece(0, 0), Rook)
+        self.assertEqual(self.board.get_piece(0, 0).color, 'BLACK')
+        self.assertIsInstance(self.board.get_piece(0, 7), Rook)
+        self.assertEqual(self.board.get_piece(0, 7).color, 'BLACK')
+        self.assertIsInstance(self.board.get_piece(7, 0), Rook)
+        self.assertEqual(self.board.get_piece(7, 0).color, 'WHITE')
+        self.assertIsInstance(self.board.get_piece(7, 7), Rook)
+        self.assertEqual(self.board.get_piece(7, 7).color, 'WHITE')
 
     def test_pawns_creation(self):
-        board = Board()
-        board.pawn_positions()  # Coloca los peones
-        self.assertIsInstance(board.get_piece(1, 0), Pawn)
-        self.assertEqual(board.get_piece(1, 0).color, 'BLACK')
-        self.assertIsInstance(board.get_piece(6, 0), Pawn)
-        self.assertEqual(board.get_piece(6, 0).color, 'WHITE')
-
-    # EMPTY SPACES ON THE BOARD WHEN STARTING THE GAME
+        self.board.pawn_positions()  # Coloca los peones
+        self.assertIsInstance(self.board.get_piece(1, 0), Pawn)
+        self.assertEqual(self.board.get_piece(1, 0).color, 'BLACK')
+        self.assertIsInstance(self.board.get_piece(6, 0), Pawn)
+        self.assertEqual(self.board.get_piece(6, 0).color, 'WHITE')
 
     def test_empty_spaces(self):
-        board = Board()
-        self.assertIsNone(board.get_piece(2, 0))
-        self.assertIsNone(board.get_piece(3, 0))
+        self.assertIsNone(self.board.get_piece(2, 0))
+        self.assertIsNone(self.board.get_piece(3, 0))
 
-    # MOVE METHOD
+    def test_move_rook(self):
+        rook = Rook(color='BLACK', board=self.board)
+        self.board.set_piece(0, 0, rook)
+        self.board.move(0, 0, 0, 1)
 
-    def test_move(self):
-        board = Board()
-        rook = Rook(color='BLACK', board=board)
-        board.set_piece(0, 0, rook)
-        board.move(0, 0, 0, 1)
-
-        self.assertIsInstance(board.get_piece(0, 1), Rook)
-        self.assertEqual(board.get_piece(0, 1).color, 'BLACK')
-        self.assertIsNone(board.get_piece(0, 0))  # Verificación de que la posición inicial queda vacía
+        self.assertIsInstance(self.board.get_piece(0, 1), Rook)
+        self.assertEqual(self.board.get_piece(0, 1).color, 'BLACK')
+        self.assertIsNone(self.board.get_piece(0, 0))  # Verifica que la posición inicial esté vacía
 
         self.assertEqual(
-            str(board),
+            str(self.board),
             (
                 ".♜.....♜\n"
                 "♟♟♟♟♟♟♟♟\n"
